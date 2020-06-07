b0a5cc5c962578e00bbd8f32f04daa631c7efe78
Chris Liu <chrisliu1999@gmail.com>
2020-06-07 16:58:55 -0700

    Added (CPU-based) BG representation for a GPU.

---
+++ src/BidirectedGraphGPU.cpp
@@ -0,0 +1,63 @@
+#include "BidirectedGraphGPU.hpp"
+
+#include <functional>
+
+BidirectedGraphGPU::BidirectedGraphGPU(HandleGraph& host_graph) {
+    copy_to_GPU(host_graph);
+}
+
+#include <iostream>
+void BidirectedGraphGPU::copy_to_GPU(HandleGraph& host_graph) {
+    size = host_graph.get_node_count();
+
+    /// Initialize node tracking lists
+    neighbor_start = (nid_t*) malloc(2 * size * sizeof(nid_t)); // Placeholder for cudaMalloc
+    neighbor_count = (nid_t*) malloc(2 * size * sizeof(nid_t)); // Placeholder for cudaMalloc
+
+    /// Get the size of adjacency 
+    size_t edges_count = 0;
+    host_graph.for_each_handle([&](const handle_t& handle) {
+        edges_count += host_graph.get_degree(handle, false);
+        edges_count += host_graph.get_degree(handle, true);
+    });
+
+    /// Populate GPU graph
+    std::function<nid_t(const handle_t&)> handle_to_GPU_index = [&](const handle_t& handle) {
+        nid_t gpu_idx = 2 * (host_graph.get_id(handle) - 1);
+        if (!host_graph.get_is_reverse(handle)) gpu_idx += 1;
+        return gpu_idx;
+    };
+
+    /// Stored as [1l, 1r, 2l, 2r, ...]
+    adjacency = (nid_t*) malloc(edges_count * sizeof(nid_t)); // Placeholder for cudaMalloc
+    /** Important:
+     * Using the assumption that node ids begin at 1 and end at get_node_count().
+     * This method is not tolerant of ids that skip. In a more robust 
+     * implementation there will be a map from GPU indices to the original 
+     * graph's node side.
+     */
+    int adjacency_i = 0;
+    for (nid_t nid = 1; nid <= size; nid++) {
+        handle_t handle = host_graph.get_handle(nid, true);
+
+        neighbor_start[2 * (nid - 1)] = adjacency_i;
+        host_graph.follow_edges(handle, false, [&](const handle_t& nei_handle) {
+            adjacency[adjacency_i] = handle_to_GPU_index(nei_handle);
+            adjacency_i++;
+        });
+        neighbor_count[2 * (nid - 1)] = host_graph.get_degree(handle, false);
+
+        neighbor_start[2 * (nid - 1) + 1] = adjacency_i;
+        host_graph.follow_edges(host_graph.flip(handle), false, [&](const handle_t& nei_handle) {
+            adjacency[adjacency_i] = handle_to_GPU_index(nei_handle);
+            adjacency_i++;
+        });
+        neighbor_count[2 * (nid - 1) + 1] = host_graph.get_degree(handle, true);
+    }
+}
+
+void BidirectedGraphGPU::dealloc() {
+    free(adjacency);
+    free(neighbor_start);
+    free(neighbor_count);
+}
---
+++ src/BidirectedGraphGPU.hpp
@@ -0,0 +1,30 @@
+#ifndef BIDIRECTEDGRAPHGPU_HPP
+#define BIDIRECTEDGRAPHGPU_HPP
+
+#include "handle.hpp"
+
+/** Each node (n_l, n_r) of node id nid will have each node side mapped to
+ * index of n_l = 2 * nid
+ * index of n_r = 2 * nid + 1
+ * We make the assumption that the size of the original graph is less than
+ * maxof(nid_t) / 2.
+ * Also assumes that node ids begin at 1 and increases sequentially.
+ */
+
+class BidirectedGraphGPU {
+private: // Hide helper functions
+    void copy_to_GPU(HandleGraph& host_graph);
+
+public: // Otherwise this is a glorified struct
+    nid_t* adjacency;      /// List of neighbors for all node sides
+    nid_t* neighbor_start; /// Where the neighbor list begins for a node side
+    nid_t* neighbor_count; /// How many neighbors for a node side 
+    size_t size;           /// NODE count of the graph
+
+
+    BidirectedGraphGPU(HandleGraph& host_graph);
+
+    void dealloc();
+};
+
+#endif /* BIDIRECTEDGRAPHGPU_HPP */
