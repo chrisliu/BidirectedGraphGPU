c65602bdbb5ecd4331223d35dad879f231790ea9
Chris Liu <chrisliu1999@gmail.com>
2020-06-07 16:58:17 -0700

    Defined handlegraph primatives and classes.

---
+++ src/handle.hpp
@@ -0,0 +1,15 @@
+#ifndef HANDLE_HPP
+#define HANDLE_HPP
+
+#include <handlegraph/handle_graph.hpp>
+#include <handlegraph/mutable_handle_graph.hpp>
+#include <handlegraph/deletable_handle_graph.hpp>
+
+using handle_t = handlegraph::handle_t;
+using nid_t = handlegraph::nid_t;
+
+using HandleGraph = handlegraph::HandleGraph;
+using MutableHandleGraph = handlegraph::MutableHandleGraph;
+using DeletableHandleGraph = handlegraph::DeletableHandleGraph;
+
+#endif /* HANDLE_HPP */
